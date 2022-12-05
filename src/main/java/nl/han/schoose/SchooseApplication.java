72bfd9ff55c5e18e4644f19d0cdad532333cfc4d
William van Schooneveld <wj.vanschooneveld@student.han.nl>
2022-12-05 11:34:34 +0100

    Commiting basic Spring Boot init

---
+++ src/main/java/nl/han/schoose/SchooseApplication.java
@@ -0,0 +1,13 @@
+package nl.han.schoose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/han/schoose/SchooseApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.han.schoose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
